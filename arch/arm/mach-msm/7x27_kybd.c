82a8f086d4175c52755e73183f8378526dffd7dd
Edgar86 <edgar_far@hotmail.com>
2012-07-12 22:20:51 +0100

    revert some changes

--- arch/arm/mach-msm/7x27_kybd.c
+++ arch/arm/mach-msm/7x27_kybd.c
@@ -1572,12 +1572,12 @@ void Q7x27_kybd_early_suspend(struct early_suspend *h)
 	{
 		enable_irq_wake(MSM_GPIO_TO_INT(g_center_pin));
 		enable_irq_wake(MSM_GPIO_TO_INT(rd->center_pin));
-		enable_irq_wake(MSM_GPIO_TO_INT(rd->volup_pin));
-		enable_irq_wake(MSM_GPIO_TO_INT(rd->voldn_pin));
-		enable_irq_wake(MSM_GPIO_TO_INT(rd->key_1_pin));
  	}
 
 
+	disable_irq(MSM_GPIO_TO_INT(rd->volup_pin));
+	disable_irq(MSM_GPIO_TO_INT(rd->voldn_pin));
+	disable_irq(MSM_GPIO_TO_INT(rd->key_1_pin));
 	disable_irq(MSM_GPIO_TO_INT(rd->key_2_pin));
 	disable_irq(MSM_GPIO_TO_INT(rd->cam_sw_t_pin));
 	disable_irq(MSM_GPIO_TO_INT(rd->cam_sw_f_pin));
@@ -1595,10 +1595,11 @@ void Q7x27_kybd_late_resume(struct early_suspend *h)
 	{
 		disable_irq_wake(MSM_GPIO_TO_INT(g_center_pin));
 		disable_irq_wake(MSM_GPIO_TO_INT(rd->center_pin));
-		disable_irq_wake(MSM_GPIO_TO_INT(rd->volup_pin));
-		disable_irq_wake(MSM_GPIO_TO_INT(rd->voldn_pin));
+
 	}
 
+	enable_irq(MSM_GPIO_TO_INT(rd->volup_pin));
+	enable_irq(MSM_GPIO_TO_INT(rd->voldn_pin));
 	enable_irq(MSM_GPIO_TO_INT(rd->key_1_pin));
 	enable_irq(MSM_GPIO_TO_INT(rd->key_2_pin));
 	enable_irq(MSM_GPIO_TO_INT(rd->volup_pin));
--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 4 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Sun Apr 22 00:56:52 WEST 2012"
-#define LINUX_COMPILE_TIME "00:56:52"
+#define UTS_VERSION "#4 PREEMPT Tue Jul 10 22:12:33 WEST 2012"
+#define LINUX_COMPILE_TIME "22:12:33"
 #define LINUX_COMPILE_BY "edgar"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILE_DOMAIN "ubuntu-domain"
-#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
+#define LINUX_COMPILE_HOST "ed-Asus"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
